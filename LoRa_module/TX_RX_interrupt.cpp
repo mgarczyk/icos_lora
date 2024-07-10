1cf1fbcf89793b31bda7bedd08698c892ebb00d6
marcel.garczyk <marcel.garczyk@pit.lukasiewicz.gov.pl>
2024-07-10 13:02:19 +0200

    TX_RX_lora with interrupt example

---
+++ LoRa_module/TX_RX_interrupt.cpp
@@ -0,0 +1,129 @@
+/**
+ * Send and receive LoRa-modulation packets with a string sequences, showing RSSI
+ * and SNR for received packets on the little display.
+ *
+ * Note that while this send and received using LoRa modulation, it does not do
+ * LoRaWAN.
+ *
+*/
+
+// Turns the 'PRG' button into the power button, long press is off 
+#define HELTEC_POWER_BUTTON   // must be before "#include <heltec_unofficial.h>"
+#include <heltec_unofficial.h>
+
+// Pause between transmited packets in seconds.
+// Set to zero to only transmit a packet when pressing the user button
+// Will not exceed 1% duty cycle, even if you set a lower value.
+#define PAUSE               300
+
+// Frequency in MHz. Keep the decimal point to designate float.
+// Check your own rules and regulations to see what is legal where you are.
+#define FREQUENCY           866.3       // for Europe
+// #define FREQUENCY           905.2       // for US
+
+// LoRa bandwidth. Keep the decimal point to designate float.
+// Allowed values are 7.8, 10.4, 15.6, 20.8, 31.25, 41.7, 62.5, 125.0, 250.0 and 500.0 kHz.
+#define BANDWIDTH           250.0
+
+// Number from 5 to 12. Higher means slower but higher "processor gain",
+// meaning (in nutshell) longer range and more robust against interference. 
+#define SPREADING_FACTOR    9
+
+// Transmit power in dBm. 0 dBm = 1 mW, enough for tabletop-testing. This value can be
+// set anywhere between -9 dBm (0.125 mW) to 22 dBm (158 mW). Note that the maximum ERP
+// (which is what your antenna maximally radiates) on the EU ISM band is 25 mW, and that
+// transmissting without an antenna can damage your hardware.
+#define TRANSMIT_POWER      0
+
+String rxdata;
+String txdata;
+volatile bool rxFlag = false;
+long counter = 0;
+uint64_t last_tx = 0;
+uint64_t tx_time;
+uint64_t minimum_pause;
+
+// Can't do Serial or display things here, takes too much time for the interrupt
+void rx_interrupt() {
+  rxFlag = true;
+}
+
+void setup() {
+  heltec_setup();
+  both.println("Radio init");
+  RADIOLIB_OR_HALT(radio.begin());
+  // Set the callback function for received packets
+  radio.setDio1Action(rx_interrupt);
+  // Set radio parameters
+  both.printf("Frequency: %.2f MHz\n", FREQUENCY);
+  RADIOLIB_OR_HALT(radio.setFrequency(FREQUENCY));
+  both.printf("Bandwidth: %.1f kHz\n", BANDWIDTH);
+  RADIOLIB_OR_HALT(radio.setBandwidth(BANDWIDTH));
+  both.printf("Spreading Factor: %i\n", SPREADING_FACTOR);
+  RADIOLIB_OR_HALT(radio.setSpreadingFactor(SPREADING_FACTOR));
+  both.printf("TX power: %i dBm\n", TRANSMIT_POWER);
+  RADIOLIB_OR_HALT(radio.setOutputPower(TRANSMIT_POWER));
+  // Start receiving
+  RADIOLIB_OR_HALT(radio.startReceive(RADIOLIB_SX126X_RX_TIMEOUT_INF));
+}
+
+void is_tx_legal(bool tx_legal){
+  if (!tx_legal) {
+      both.printf("Legal limit, wait %i sec.\n", (int)((minimum_pause - (millis() - last_tx)) / 1000) + 1);
+      return;
+    }
+}
+
+void tx(){
+  both.printf("TX [%s] ", txdata);
+  radio.clearDio1Action();
+  heltec_led(50); // 50% brightness is plenty for this LED
+  tx_time = millis();
+  RADIOLIB(radio.transmit(txdata));
+  tx_time = millis() - tx_time;
+  heltec_led(0);
+  if (_radiolib_status == RADIOLIB_ERR_NONE) {
+      both.printf("OK (%i ms)\n", (int)tx_time);
+  } else {
+      both.printf("fail (%i)\n", _radiolib_status);
+  }
+}
+
+void set_up_duty_cycle(){
+  minimum_pause = tx_time * 100;
+  last_tx = millis();
+  radio.setDio1Action(rx_interrupt);
+  RADIOLIB_OR_HALT(radio.startReceive(RADIOLIB_SX126X_RX_TIMEOUT_INF));
+}
+
+
+  // If a packet was received, display it and the RSSI and SNR
+bool rx(bool rxFlag){
+ if (rxFlag) {
+    rxFlag = false;
+    radio.readData(rxdata);
+    if (_radiolib_status == RADIOLIB_ERR_NONE) {
+      both.printf("RX [%s]\n", rxdata.c_str());
+      both.printf("  RSSI: %.2f dBm\n", radio.getRSSI());
+      both.printf("  SNR: %.2f dB\n", radio.getSNR());
+    }
+    RADIOLIB_OR_HALT(radio.startReceive(RADIOLIB_SX126X_RX_TIMEOUT_INF));
+  }
+  return rxFlag;
+}
+
+void loop() {
+  heltec_loop();
+  bool tx_legal = millis() > last_tx + minimum_pause;
+  // Transmit a packet every PAUSE seconds or when someting is on UART module
+  if ((PAUSE && tx_legal && millis() - last_tx > (PAUSE * 1000)) || Serial.available() > 0) {
+    txdata = Serial.readStringUntil('\n');
+    // If we want to send something to soon, then we get that info.
+    is_tx_legal(tx_legal);
+    tx();
+    set_up_duty_cycle();
+  }
+  rxFlag = rx(rxFlag);
+}
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,83 +1,132 @@
-/*
-  Code Base from RadioLib: https://github.com/jgromes/RadioLib/tree/master/examples/SX126x
-
-  For full API reference, see the GitHub Pages
-  https://jgromes.github.io/RadioLib/
+/**
+ * Send and receive LoRa-modulation packets with a sequence number, showing RSSI
+ * and SNR for received packets on the little display.
+ *
+ * Note that while this send and received using LoRa modulation, it does not do
+ * LoRaWAN. For that, see the LoRaWAN_TTN example.
+ *
+ * This works on the stick, but the output on the screen gets cut off.
 */
 
-#include <Arduino.h>
-#include <SPI.h>
-#include <Wire.h>
-#include <RadioLib.h>
 
-#define LoRa_MOSI 10
-#define LoRa_MISO 11
-#define LoRa_SCK 9
 
-#define LoRa_nss 8
-#define LoRa_dio1 14
-#define LoRa_nrst 12
-#define LoRa_busy 13
+// Turns the 'PRG' button into the power button, long press is off 
+#define HELTEC_POWER_BUTTON   // must be before "#include <heltec_unofficial.h>"
+#include <heltec_unofficial.h>
 
-SX1262 radio = new Module(LoRa_nss, LoRa_dio1, LoRa_nrst, LoRa_busy);
+// Pause between transmited packets in seconds.
+// Set to zero to only transmit a packet when pressing the user button
+// Will not exceed 1% duty cycle, even if you set a lower value.
+#define PAUSE               300
 
-void setup()
-{
-  Serial.begin(250000); // Initialize serial communication for UART
-  SPI.begin(LoRa_SCK, LoRa_MISO, LoRa_MOSI, LoRa_nss);
+// Frequency in MHz. Keep the decimal point to designate float.
+// Check your own rules and regulations to see what is legal where you are.
+#define FREQUENCY           866.3       // for Europe
+// #define FREQUENCY           905.2       // for US
 
-  // initialize SX1262 with default settings
-  Serial.print(F("[SX1262] Initializing ... "));
-  int state = radio.begin();
-  if (state == RADIOLIB_ERR_NONE)
-  {
-    Serial.println(F("success!"));
-  }
-  else
-  {
-    Serial.print(F("failed, code "));
-    Serial.println(state);
-    while (true);
+// LoRa bandwidth. Keep the decimal point to designate float.
+// Allowed values are 7.8, 10.4, 15.6, 20.8, 31.25, 41.7, 62.5, 125.0, 250.0 and 500.0 kHz.
+#define BANDWIDTH           250.0
+
+// Number from 5 to 12. Higher means slower but higher "processor gain",
+// meaning (in nutshell) longer range and more robust against interference. 
+#define SPREADING_FACTOR    9
+
+// Transmit power in dBm. 0 dBm = 1 mW, enough for tabletop-testing. This value can be
+// set anywhere between -9 dBm (0.125 mW) to 22 dBm (158 mW). Note that the maximum ERP
+// (which is what your antenna maximally radiates) on the EU ISM band is 25 mW, and that
+// transmissting without an antenna can damage your hardware.
+#define TRANSMIT_POWER      0
+
+String rxdata;
+String txdata;
+volatile bool rxFlag = false;
+long counter = 0;
+uint64_t last_tx = 0;
+uint64_t tx_time;
+uint64_t minimum_pause;
+
+// Can't do Serial or display things here, takes too much time for the interrupt
+void rx_interrupt() {
+  rxFlag = true;
+}
+
+void setup() {
+  heltec_setup();
+  both.println("Radio init");
+  RADIOLIB_OR_HALT(radio.begin());
+  // Set the callback function for received packets
+  radio.setDio1Action(rx_interrupt);
+  // Set radio parameters
+  both.printf("Frequency: %.2f MHz\n", FREQUENCY);
+  RADIOLIB_OR_HALT(radio.setFrequency(FREQUENCY));
+  both.printf("Bandwidth: %.1f kHz\n", BANDWIDTH);
+  RADIOLIB_OR_HALT(radio.setBandwidth(BANDWIDTH));
+  both.printf("Spreading Factor: %i\n", SPREADING_FACTOR);
+  RADIOLIB_OR_HALT(radio.setSpreadingFactor(SPREADING_FACTOR));
+  both.printf("TX power: %i dBm\n", TRANSMIT_POWER);
+  RADIOLIB_OR_HALT(radio.setOutputPower(TRANSMIT_POWER));
+  // Start receiving
+  RADIOLIB_OR_HALT(radio.startReceive(RADIOLIB_SX126X_RX_TIMEOUT_INF));
+}
+
+void is_tx_legal(bool tx_legal){
+  if (!tx_legal) {
+      both.printf("Legal limit, wait %i sec.\n", (int)((minimum_pause - (millis() - last_tx)) / 1000) + 1);
+      return;
+    }
+}
+
+void tx(){
+  both.printf("TX [%s] ", txdata);
+  radio.clearDio1Action();
+  heltec_led(50); // 50% brightness is plenty for this LED
+  tx_time = millis();
+  RADIOLIB(radio.transmit(txdata));
+  tx_time = millis() - tx_time;
+  heltec_led(0);
+  if (_radiolib_status == RADIOLIB_ERR_NONE) {
+      both.printf("OK (%i ms)\n", (int)tx_time);
+  } else {
+      both.printf("fail (%i)\n", _radiolib_status);
   }
 }
 
-void loop()
-{
-  if (Serial.available() > 0)
-  {
-    // Read data from UART
-    String uartData = Serial.readStringUntil('\n');
-
-    Serial.print(F("[SX1262] Transmitting packet ... "));
-
-    // Transmit the data over LoRa
-    int state = radio.transmit(uartData);
-
-    if (state == RADIOLIB_ERR_NONE)
-    {
-      // the packet was successfully transmitted
-      Serial.println(F("success!"));
-
-      // print measured data rate
-      Serial.print(F("[SX1262] Datarate:\t"));
-      Serial.print(radio.getDataRate());
-      Serial.println(F(" bps"));
-    }
-    else if (state == RADIOLIB_ERR_PACKET_TOO_LONG)
-    {
-      // the supplied packet was longer than 256 bytes
-      Serial.println(F("too long!"));
-    }
-    else if (state == RADIOLIB_ERR_TX_TIMEOUT)
-    {
-      // timeout occurred while transmitting packet
-      Serial.println(F("timeout!"));
-    }
-    else
-    {
-      // some other error occurred
-      Serial.print(F("failed, code "));
-      Serial.println(state);
-    }
-  }
+void set_up_duty_cycle(){
+  minimum_pause = tx_time * 100;
+  last_tx = millis();
+  radio.setDio1Action(rx_interrupt);
+  RADIOLIB_OR_HALT(radio.startReceive(RADIOLIB_SX126X_RX_TIMEOUT_INF));
 }
+
+
+  // If a packet was received, display it and the RSSI and SNR
+bool rx(bool rxFlag){
+ if (rxFlag) {
+    rxFlag = false;
+    radio.readData(rxdata);
+    if (_radiolib_status == RADIOLIB_ERR_NONE) {
+      both.printf("RX [%s]\n", rxdata.c_str());
+      both.printf("  RSSI: %.2f dBm\n", radio.getRSSI());
+      both.printf("  SNR: %.2f dB\n", radio.getSNR());
+    }
+    RADIOLIB_OR_HALT(radio.startReceive(RADIOLIB_SX126X_RX_TIMEOUT_INF));
+  }
+  return rxFlag;
+}
+
+void loop() {
+  heltec_loop();
+  bool tx_legal = millis() > last_tx + minimum_pause;
+  // Transmit a packet every PAUSE seconds or when someting is on UART module
+  if ((PAUSE && tx_legal && millis() - last_tx > (PAUSE * 1000)) || Serial.available() > 0) {
+    txdata = Serial.readStringUntil('\n');
+    // If we want to send something to soon, then we get that info.
+    is_tx_legal(tx_legal);
+    tx();
+    set_up_duty_cycle();
+  }
+  rxFlag = rx(rxFlag);
+}
+
+
