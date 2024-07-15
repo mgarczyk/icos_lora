8ae1e99a9d6e11a8cebc4b34190de5229068859d
marcel.garczyk <marcel.garczyk@pit.lukasiewicz.gov.pl>
2024-07-15 12:02:32 +0200

    MCU program only RX data show on display.

---
+++ LoRa_module/TX_RX_interrupt_OLED_RX.cpp
@@ -0,0 +1,120 @@
+/**
+ * Send and receive LoRa-modulation packets with a sequence number, showing RSSI
+ * and SNR for received packets on the little display. 
+ * This version shows only RX data on the screen.
+ *
+ * Note that while this send and received using LoRa modulation, it does not do
+ * LoRaWAN. For that, see the LoRaWAN_TTN example.
+ *
+ * This works on the stick, but the output on the screen gets cut off.
+*/
+
+
+
+// Turns the 'PRG' button into the power button, long press is off 
+#define HELTEC_POWER_BUTTON   // must be before "#include <heltec_unofficial.h>"
+#include <heltec_unofficial.h>
+
+// Pause between transmited packets in seconds.
+// Set to zero to only transmit a packet when pressing the user button
+// Will not exceed 1% duty cycle, even if you set a lower value.
+#define PAUSE               300
+
+// Frequency in MHz. Keep the decimal point to designate float.
+// Check your own rules and regulations to see what is legal where you are.
+#define FREQUENCY           866.3       // for Europe
+// #define FREQUENCY           905.2       // for US
+
+// LoRa bandwidth. Keep the decimal point to designate float.
+// Allowed values are 7.8, 10.4, 15.6, 20.8, 31.25, 41.7, 62.5, 125.0, 250.0 and 500.0 kHz.
+#define BANDWIDTH           250.0
+
+// Number from 5 to 12. Higher means slower but higher "processor gain",
+// meaning (in nutshell) longer range and more robust against interference. 
+#define SPREADING_FACTOR    9
+
+// Transmit power in dBm. 0 dBm = 1 mW, enough for tabletop-testing. This value can be
+// set anywhere between -9 dBm (0.125 mW) to 22 dBm (158 mW). Note that the maximum ERP
+// (which is what your antenna maximally radiates) on the EU ISM band is 25 mW, and that
+// transmissting without an antenna can damage your hardware.
+#define TRANSMIT_POWER      0
+
+String rxdata;
+String txdata;
+volatile bool rxFlag = false;
+long counter = 0;
+uint64_t last_tx = 0;
+uint64_t tx_time;
+uint64_t minimum_pause;
+
+// Can't do Serial or display things here, takes too much time for the interrupt
+void rx_interrupt() {
+  rxFlag = true;
+}
+
+void setup() {
+  heltec_setup();
+  RADIOLIB_OR_HALT(radio.begin());
+  // Set the callback function for received packets
+  radio.setDio1Action(rx_interrupt);
+  // Set radio parameters
+  RADIOLIB_OR_HALT(radio.setFrequency(FREQUENCY));
+  RADIOLIB_OR_HALT(radio.setBandwidth(BANDWIDTH));
+  RADIOLIB_OR_HALT(radio.setSpreadingFactor(SPREADING_FACTOR));
+  RADIOLIB_OR_HALT(radio.setOutputPower(TRANSMIT_POWER));
+  // Start receiving
+  RADIOLIB_OR_HALT(radio.startReceive(RADIOLIB_SX126X_RX_TIMEOUT_INF));
+}
+
+void is_tx_legal(bool tx_legal){
+  if (!tx_legal) {
+      return;
+    }
+}
+
+void tx(){
+  both.printf("%s", txdata);
+  radio.clearDio1Action();
+  heltec_led(50); // 50% brightness is plenty for this LED
+  tx_time = millis();
+  RADIOLIB(radio.transmit(txdata));
+  tx_time = millis() - tx_time;
+  heltec_led(0);
+}
+
+void set_up_duty_cycle(){
+  minimum_pause = tx_time * 400;
+  last_tx = millis();
+  radio.setDio1Action(rx_interrupt);
+  RADIOLIB_OR_HALT(radio.startReceive(RADIOLIB_SX126X_RX_TIMEOUT_INF));
+}
+
+
+  // If a packet was received, display it and the RSSI and SNR
+bool rx(bool rxFlag){
+ if (rxFlag) {
+    rxFlag = false;
+    radio.readData(rxdata);
+    if (_radiolib_status == RADIOLIB_ERR_NONE) {
+      both.printf("%s\n", rxdata.c_str());
+    }
+    RADIOLIB_OR_HALT(radio.startReceive(RADIOLIB_SX126X_RX_TIMEOUT_INF));
+  }
+  return rxFlag;
+}
+
+void loop() {
+  heltec_loop();
+  bool tx_legal = millis() > last_tx + minimum_pause;
+  // Transmit a packet every PAUSE seconds or when someting is on UART module
+  if ((PAUSE && tx_legal && millis() - last_tx > (PAUSE * 1000)) || Serial.available() > 0) {
+    txdata = Serial.readStringUntil('\n');
+    // If we want to send something to soon, then we get that info.
+    is_tx_legal(tx_legal);
+    tx();
+    set_up_duty_cycle();
+  }
+  rxFlag = rx(rxFlag);
+}
+
+
